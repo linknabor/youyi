65213cf9932324a172bab7ed94808364144b5a37
jackie <liuzj_jj@163.com>
2017-07-20 14:32:09 +0800

    添加https支持

--- hexie/src/main/java/com/yumu/hexie/common/config/AppConfig.java
+++ hexie/src/main/java/com/yumu/hexie/common/config/AppConfig.java
@@ -84,7 +84,7 @@ public class AppConfig {
     }
     
     /**
-     * httpsè®¿é—®
+     * https·ÃÎÊ
      * @return
      */
     private Connector createSslConnector() {
@@ -95,7 +95,7 @@ public class AppConfig {
             connector.setScheme("https");
             protocol.setSSLEnabled(true);
             connector.setSecure(true);
-            connector.setPort(443);
+            connector.setPort(8443);
             protocol.setKeystoreFile(truststore.getAbsolutePath());
             protocol.setKeystorePass("hongzhitech20130110");
 //            protocol.setKeyAlias("springboot");
