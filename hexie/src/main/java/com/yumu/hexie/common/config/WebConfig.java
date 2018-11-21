6438a6b4c6d8d0ce2bf53e76f3b5607f5cbc7f89
davidhardson <davidhardson@hotmail.com>
2018-11-21 17:38:50 +0800

    upgrade spring boot version from 1.3.5Release to 1.5.17Release

--- hexie/src/main/java/com/yumu/hexie/common/config/WebConfig.java
+++ hexie/src/main/java/com/yumu/hexie/common/config/WebConfig.java
@@ -27,7 +27,6 @@ import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -52,11 +51,6 @@ public class WebConfig extends WebMvcConfigurationSupport {
         return handlerMapping;
     }
 
-    @Override
-    @Bean
-    public HandlerMapping resourceHandlerMapping() {
-        return super.resourceHandlerMapping();
-    }
     protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
     	converters.clear();
     	MappingJackson2HttpMessageConverter c = new MappingJackson2HttpMessageConverter(){
